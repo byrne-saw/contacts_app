9ffaa437ff86450f278d29f29f220bd52f6d3911
byrne-saw <byrnes.aw@gmail.com>
2018-09-01 14:05:49 -0500

    finishing day 10 hw

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
     get "/contacts" => 'contacts#index'
     post "/contacts" => 'contacts#create'
     get "/contacts/:id" => 'contacts#show'
-
+    patch "/contacts/:id" => 'contacts#update'
+    delete "/contacts/:id" => 'contacts#destroy'
   end
 end
